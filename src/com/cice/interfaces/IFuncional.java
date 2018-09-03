d7431d35e7d76883c84d2b431247ab31fbf7a257
zalcanizr <39500802+zalcanizr@users.noreply.github.com>
2018-09-03 19:39:14 +0200

    Update IFuncional.java

--- src/com/cice/interfaces/IFuncional.java
+++ src/com/cice/interfaces/IFuncional.java
@@ -2,7 +2,10 @@ package com.cice.interfaces;
 
 @FunctionalInterface
 
-//interfaz que tienen un unico metodo sin definir pero puede tener muchos más metodos definidos
+//interfaz funcional es aquella que tienen un unico metodo sin definir pero puede tener muchos más metodos definidos
+//la expresion landa se apoya en una interfaz funcional, la expresion define la funcionalidad del método abstracto, 
+//por eso solo tengo un metodo abstracto porque si tuvieramos varios sin definir no sabría a cúal método 
+//le está definiendo la funcionalidad.
 
 public interface IFuncional {
 
